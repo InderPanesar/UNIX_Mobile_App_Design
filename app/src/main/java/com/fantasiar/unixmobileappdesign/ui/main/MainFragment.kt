6ac9de94d8b4f3a08155ccefbc0204f72d95ecc0
Inder Panesar <Inder_Panesar@hotmail.com>
2022-07-30 22:06:17 +0100

    Creating Initial Page

--- app/src/main/java/com/fantasiar/unixmobileappdesign/ui/main/MainFragment.kt
+++ app/src/main/java/com/fantasiar/unixmobileappdesign/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.fantasiar.unixmobileappdesign.MainActivity
 import com.fantasiar.unixmobileappdesign.R
 
 class MainFragment : Fragment() {
@@ -25,6 +26,7 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        (requireActivity() as MainActivity).supportActionBar!!.hide()
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
