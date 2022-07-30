8b6671dc670ae5cc9a046c8fef1acdc303fce0ae
Inder Panesar <Inder_Panesar@hotmail.com>
2022-07-30 20:53:28 +0100

    Project Initialization

---
+++ app/src/main/java/com/fantasiar/unixmobileappdesign/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.fantasiar.unixmobileappdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fantasiar.unixmobileappdesign.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
